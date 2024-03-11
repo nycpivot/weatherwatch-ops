514a45cdf603419fc8a1782d8c7092dbef69ee6e
nycpivot <developer@thejameshome.net>
2024-03-11 14:55:00 -0400

    Organized folders and files.

--- api-operations/04-dapr-asb/01-asb-install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-resource_group=weatherwatch
-namespace=weatherwatch
-
-az servicebus namespace create --name $namespace --resource-group $resource_group --location eastus
-
--- api-operations/dapr/01-dapr-install.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-kubectl config use-context weatherwatch-api
-
-dapr init -k --dev
-
-dapr dashboard -k
-
